a87ede283504cd585c819a476b36f73536a7daff
iwyu02ZYF*sP <github_cloudlabsuser_1319@cloudlabsaiuser.com>
2025-03-27 07:02:43 +0000

    Revert "fix"
    This reverts commit ea4c49689fffadfffa98c20991e5c075333be52d.

--- src/ContosoTraders.Api.Core/Requests/Definitions/LoadTestRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace ContosoTraders.Api.Core.Requests.Definitions;
-
-public class LoadTestRequest : IRequest<IActionResult>
-{
-}
--- src/ContosoTraders.Ui.Website/src/pages/list/sections/index.js
+++
@@ -1,8 +0,0 @@
-import ListAside from './listAside/listAside';
-import ListGrid from './listGrid/listGrid';
-import OfferBanner from './banner/offerBanner';
-export {
-    ListAside,
-    ListGrid,
-    OfferBanner,
-};
